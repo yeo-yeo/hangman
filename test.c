d199e88c35ecaeb6dfc320a0cd3dafe8e091cd9f
Gillian Yeomans <gillian@cord.com>
2023-05-13 18:31:04 +0100

    get you to enter input each turn and display word as you go along

--- test.c
+++ test.c
@@ -2,7 +2,9 @@
 #include <stdbool.h>
 #include <cs50.h>
 
-int checkIfWon(char* wordToGuess, char* alreadyGuessed);
+bool checkIfWon(char* wordToGuess, char* alreadyGuessed);
+bool haveGuessedLetter(char letter, char* alreadyGuessed);
+void add_guess_to_already_guessed(char guess, char* already_guessed);
 
 int main(void) {
     printf("Welcome! Let's play hangman.\n");
@@ -12,20 +14,52 @@ int main(void) {
     
     char wordToGuess[] = "rolo";
 
-    int remainingTurns = 10;
+    int total_turns = 10;
+    // is this going to assoc reference
+    int remainingTurns = total_turns;
     bool hasWon = false;
 
     char alreadyGuessed[10];
 
-    char *guess = get_string("Next guess please: ");
+
+    // this seems very dumb
+    for (int i = 0; i < total_turns; i++) {
+        alreadyGuessed[i] = '?';
+    }
 
     while (hasWon == false && remainingTurns > 0) {
-    	checkIfWon(wordToGuess, alreadyGuessed); 
+
+	printf("Current state: ");
+	
+// loop through word to guess and display __ or guessed letters    
+	for (int i = 0; wordToGuess[i] != '\0'; i++) {
+	      if (haveGuessedLetter(wordToGuess[i], alreadyGuessed)) {
+		    printf("%c ", wordToGuess[i]);
+		 } else {
+		 printf("_ ");
+		 }
+	    }
+	    printf("\n");;
+
+	    // get next turn's input
+// need to do something to make them only enter one letter
+    	    char *guess = get_string("Next guess please: ");
+	
+	// add it to the array
+	add_guess_to_already_guessed(guess[0], alreadyGuessed);
+	
+
+	// check if they've won
+	hasWon = checkIfWon(wordToGuess, alreadyGuessed); 
 	remainingTurns--;
     }
 
     if (hasWon) {
     printf("You win!");
+    }
+
+    if (!hasWon && remainingTurns == 0) {
+    printf("You lose!");
     }
 
     printf("    _____    \n    |   !   \n    |   O   \n    |  /|\\  \n    | / | \\  \n    |  / \\  \n    | /   \\\n____|____  \n");
@@ -34,7 +68,24 @@ int main(void) {
 
 }
 
-int checkIfWon(char* wordToGuess, char* alreadyGuessed) {
+bool checkIfWon(char* wordToGuess, char* alreadyGuessed) {
 	printf("Checking...\n");
-	return 1;
+	return false;
+}
+
+bool haveGuessedLetter(char letter, char* alreadyGuessed) {
+	for (int i = 0; i < sizeof(alreadyGuessed); i++) {
+	    if (letter == alreadyGuessed[i]) {
+	    return true;
+	    } 
+	}
+	return false;
+}
+void add_guess_to_already_guessed(char guess, char* already_guessed) {
+	for (int i = 0; i < sizeof(already_guessed); i++) {
+	    if (already_guessed[i] == '?') {
+		already_guessed[i] = guess;
+	        break;
+	}
+	}
 }
