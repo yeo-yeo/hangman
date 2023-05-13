42a8d6dfae80bb10a348bb29ac75bfc0e7dba626
Gillian Yeomans <gillian@cord.com>
2023-05-13 13:56:11 +0100

    ascii art

---
+++ test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Welcome! Let's play hangman.\n");
+
+    // run fn that chooses word.. it returns word (and letter count? not really nec)
+    // fn that does a turn... inputs: word, remaining no of turns, guesses so far
+    
+    int remainingTurns = 10;
+    
+
+
+    while (hasWon == false && remainingTurns > 0) {
+    	takeTurn(wordToGuess, alreadyGuessed) // 
+	remainingTurns--;
+    }
+
+    if (
+    printf("You win!);
+
+    printf("    _____    \n    |   !   \n    |   O   \n    |  /|\\  \n    | / | \\  \n    |  / \\  \n    | /   \\\n____|____  \n");
+}
